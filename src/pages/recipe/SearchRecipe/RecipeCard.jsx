a0c27dc46d6cc7c8e166b756d0b5346817761958
tova <36213354335@mby.co.il>
2023-04-29 22:39:11 +0300

    try

--- src/pages/recipe/SearchRecipe/RecipeCard.jsx
+++ src/pages/recipe/SearchRecipe/RecipeCard.jsx
@@ -13,26 +13,23 @@ const RecipeCard = ({ recipe, src, setRecipes }) => {
     return (
         <Card onClick={() => alert("card clicked")} variant="outlined"
             sx={{
-                minWidth: "320px",maxWidth:"400px", m: 3, borderWidth: 0,
+                minWidth: "320px", maxWidth: "320px", m: 2, borderWidth: 0,
                 '&:hover': { boxShadow: '6px 6px 2px 1px rgba(189, 104, 109, .2)' },
             }}>
             <CardOverflow>
                 <AspectRatio ratio="1.75">
                     <img border="none" outline="none"
-                        // src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318"
                         src={recipe.image}
                         loading="lazy"
                         alt=""
                     />
                 </AspectRatio>
-                {src == "api" ? <ApiCard/> : <SpoonacularCard />}
+                {src == "api" ? <ApiCard /> : <SpoonacularCard />}
             </CardOverflow>
             <div style={{ height: 120 }}>
-                <Typography level="h2" sx={{ fontSize: '32', mt: 2, fontWeight: '10' }}>{recipe?.title}</Typography>
-                {/* <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical', maxHeight: '3.6em' }}>
-                    <Typography level="h6" sx={{ mt: 0.5, mb: 2, fontWeight: '20' }}>{recipe?.description}</Typography>
-                </div> */}
-                <Typography level="h6" sx={{ mt: 0.5, mb: 2, fontWeight: '20', }}>{recipe?.description}</Typography>
+                <Typography level="h4" sx={{ fontSize: '32', mt: 2, fontWeight: '10' }}>
+                    {src == "api" ? recipe.name : recipe?.title}
+                </Typography>
             </div>
             <Divider />
             <CardOverflow
@@ -54,7 +51,7 @@ const RecipeCard = ({ recipe, src, setRecipes }) => {
                 </Typography>
                 <Divider orientation="vertical" />
                 <Typography level="body1" sx={{ fontWeight: '20', color: 'text.secondary' }}>
-                   {src == "api" ?  <span>&#128507; {recipe.difficult}</span>:<span>&#128077; {recipe.aggregateLikes} likes</span>}
+                    {src == "api" ? <span>&#128507; {recipe.difficult}</span> : <span>&#128077; {recipe.aggregateLikes} likes</span>}
                 </Typography>
             </CardOverflow>
         </Card>
