c741fe3cf30d1ed4c0ff570dca76c33450adf8fc
NechamaDonnebaum <nechama8057@gmail.com>
2023-07-24 12:38:42 +0300

    .

--- src/pages/ShowApiRecipe/index.jsx
+++ src/pages/ShowApiRecipe/index.jsx
@@ -41,7 +41,7 @@ function ShowApiRecipe() {
           <img
             src={`http://localhost:3600/images/${recipe.img}`}
             style={{ width: "100%",            margin: "auto",
-          }}
+            }}
           ></img>
         </Box>
         <Details recipe={recipe} />
